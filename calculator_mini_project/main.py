19235c7297f44fa55aa78d848539f7dddbac7f5e
Mohamed Sayed <38618689-mohaamdsayed@users.noreply.replit.com>
2024-12-29 18:14:57 +0000

    calculator mini projecr

---
+++ calculator_mini_project/main.py
@@ -0,0 +1,14 @@
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
+sum =first_number+second_number
+sub=first_number-second_number
+mul=first_number*second_number
+div=first_number/second_number
+print(
+  f"""
+  The sum is {sum}.
+  The subtraction is {sub}.
+  The multiplication is {mul}.
+  The division is {div}.
+  """
+)
