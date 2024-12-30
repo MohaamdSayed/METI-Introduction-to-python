788513f9dfa0b6771c60f447eb3f902200028347
Mohamed Sayed <38618689-mohaamdsayed@users.noreply.replit.com>
2024-12-30 16:46:49 +0000

    prime number mini project

---
+++ prime_mini_project/main.py
@@ -0,0 +1,15 @@
+range_end = int(input("enter the range end: "))
+
+numbers =[True]*range_end
+numbers[0]=False
+numbers[1]=False
+
+for i in range(2, range_end):
+  if(numbers[i]):
+    print(i)
+    for j in range (2,range_end):
+      num = j * i
+      if(num>=range_end):
+        break
+      else:
+        numbers[num]=False
